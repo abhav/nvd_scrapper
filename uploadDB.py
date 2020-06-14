0e76894905de411f8ff7de63d49746f54ee57dca
Abhav Luthra <abhavluthra18@gmail.com>
2020-06-14 14:30:01 -0500

    Version 1.1 ----------------------------------- Converted coded to OO

--- uploadDB.py
+++ uploadDB.py
@@ -1,38 +1,38 @@
 import psycopg2
 import csv
-import sys
 from configDB import sql_conn
 
-conn = psycopg2.connect(sql_conn)
 
-cur = conn.cursor()
-if sys.argv[1] == '1':
-    cur.execute("TRUNCATE nvd, cvss, cpe_version, product CASCADE;")
-    print('All Tables Truncated')
+class AddRecord:
+    conn = psycopg2.connect(sql_conn)
+    cur = conn.cursor()
+    def truncatePrevious(self):
+        self.cur.execute("TRUNCATE nvd, cvss, cpe_version, product CASCADE;")
+        print('All Tables Truncated')
 
+    def insertRecord(self):
+        with open('nvd_data.csv', 'r') as f:
+            reader = csv.reader(f)
+            for row in reader:
+                self.cur.execute("INSERT INTO nvd VALUES (%s, %s, %s, %s);", row)
+        print('NVD transferred successfully ')
 
-with open('nvd_data.csv', 'r') as f:
-    reader = csv.reader(f)
-    for row in reader:
-        cur.execute("INSERT INTO nvd VALUES (%s, %s, %s, %s);", row)
-print('NVD transferred successfully ')
+        with open('cvss_data.csv', 'r') as f:
+            reader = csv.reader(f)
+            for row in reader:
+                self.cur.execute("INSERT INTO cvss VALUES (%s, %s, %s);", row)
+        print('CVSS transferred successfully ')
 
-with open('cvss_data.csv', 'r') as f:
-    reader = csv.reader(f)
-    for row in reader:
-        cur.execute("INSERT INTO cvss VALUES (%s, %s, %s);", row)
-print('CVSS transferred successfully ')
+        with open('cpe_data.csv', 'r') as f:
+            reader = csv.reader(f)
+            for row in reader:
+                self.cur.execute("INSERT INTO cpe_version VALUES (%s, %s, %s, %s, %s, %s);", row)
+        print('CPE transferred successfully ')
 
-with open('cpe_data.csv', 'r') as f:
-    reader = csv.reader(f)
-    for row in reader:
-        cur.execute("INSERT INTO cpe_version VALUES (%s, %s, %s, %s, %s, %s);", row)
-print('CPE transferred successfully ')
-
-with open('product_data.csv', 'r') as f:
-    reader = csv.reader(f)
-    for row in reader:
-        cur.execute("INSERT INTO product VALUES (%s, %s, %s, %s);", row)
-print('Product transferred successfully ')
-
-conn.commit()
+        with open('product_data.csv', 'r') as f:
+            reader = csv.reader(f)
+            for row in reader:
+                self.cur.execute("INSERT INTO product VALUES (%s, %s, %s, %s);", row)
+        print('Product transferred successfully ')
+        self.conn.commit()
+        self.conn.close()
